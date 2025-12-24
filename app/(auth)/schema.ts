c36a65b3b1d9592a62d24dee97c6db2ec9bb505a
Aryan <aaryan.nakarmi111@gmail.com>
2025-12-24 20:29:39 +0545

    Created Login and Registration Schema

--- app/(auth)/schema.ts
+++ app/(auth)/schema.ts
@@ -4,3 +4,15 @@ export const loginSchema = z.object({
     email: z.email({message:"Enter a valid email"}),
     password: z.string().min(6,{message:"Minimum 6 characters"}),
 });
+
+export type LoginData = z.infer<typeof loginSchema>;
+
+export const registerSchema = z.object({
+    name: z.string().min(2,{message:"Enter you name"}),
+    email: z.email({message: "Enter a valid email"}),
+    password: z.string().min(6,{message:"Minimum 6 characters"}),
+     confirmPassword: z.string().min(6, { message: "Minimum 6 characters" }),
+}).refine((v)=> v.password === v.confirmPassword,{
+    path: ["confirmPassword"],
+    message: "Passwords do not match",
+});
