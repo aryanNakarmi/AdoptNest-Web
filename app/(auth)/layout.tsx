9798d4efb857bc7eaefb2c70c6d8e724f64b5f54
Aryan <aaryan.nakarmi111@gmail.com>
2025-12-25 18:01:01 +0545

    Website Logo added

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,10 +1,22 @@
+import Image from "next/image";
+
 export default function AuthLayout({children}: {children: React.ReactNode}) {
     return (
         
         <div className="relative flex min-h-screen w-full items-center justify-center bg-background-light dark:bg-background-dark p-4">
+            
          <header className="absolute top-0 left-0 right-0 p-4">
-            <div className="flex items-center gap-2 ">
-                <h2 className="text-2xl font-bold">AdoptNest</h2>
+            <div className="flex items-center gap-2 ">  
+               
+                <Image 
+                src="/images/logo.png"
+                alt="Adoptnest logo"
+                width={50}
+                height={30}
+                className="object-cover"
+                />
+            
+                <h2 className=" font-bold">AdoptNest</h2>
             </div>
          </header>
 
