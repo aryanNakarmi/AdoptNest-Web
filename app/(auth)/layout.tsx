6c4c41aff0be80afb984696486463024a6b79574
Aryan <aaryan.nakarmi111@gmail.com>
2025-12-25 18:30:30 +0545

    Auth Layout

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -3,26 +3,28 @@ import Image from "next/image";
 export default function AuthLayout({children}: {children: React.ReactNode}) {
     return (
         
-        <div className="relative flex min-h-screen w-full items-center justify-center bg-background-light dark:bg-background-dark p-4">
+        <div className="relative flex min-h-screen w-full items-center justify-center bg-black p-4">
             
-         <header className="absolute top-0 left-0 right-0 p-4">
-            <div className="flex items-center gap-2 ">  
-               
-                <Image 
-                src="/images/logo.png"
-                alt="Adoptnest logo"
-                width={50}
-                height={30}
-                className="object-cover"
-                />
-            
-                <h2 className=" font-bold">AdoptNest</h2>
-            </div>
-         </header>
+   
 
-        <section>
-            {children}
-        </section>
+         <div className="grid h-full w-full max-w-4xl grid-cols-1 rounded-xl bg-white/50 dark:bg-black/20 shadow-lg ring-1 ring-black/5 md:grid-cols-2">
+        
+        {/* Left: Illustration */}
+        <div className="relative hidden md:block">
+          <Image
+            src="/images/image3.png"
+            alt="Happy pet illustration"
+            fill
+            className="object-cover rounded-l-xl"
+          />
+        </div>
+
+     
+        <div className="flex flex-col items-center justify-center p-8 sm:p-12">
+          {children}
+        </div>
+
+      </div>
         </div>
     );
 }
