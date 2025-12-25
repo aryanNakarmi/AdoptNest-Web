f12c232205c591d3e3a4abd89a4123cec12c3970
Aryan <aaryan.nakarmi111@gmail.com>
2025-12-25 21:03:07 +0545

    Login Page Created

--- app/(auth)/_components/LoginForm.tsx
+++ app/(auth)/_components/LoginForm.tsx
@@ -32,18 +32,48 @@ export default function LoginForm(){
 
     return(
         <form onSubmit={handleSubmit(submit)}>
-            <div>
-                <div>
+            <div className="space-y-4">
+                <div className="font-medium">
                     <label htmlFor="email">Email</label>
                     <input type="email"
                     id="email"
+                    placeholder="youremail@example.com"
                     autoComplete="email" 
+                    className="w-full px-4 h-12 rounded-lg 
+                    border border-gray-400
+                    text-white
+                    focus:border-foreground/30"
                     {...register("email")}/>
                     {errors.email?.message && (
                         <p
                         className="text-xs text-red-600">{errors.email.message}</p>
                     )}
                 </div>
+                <div className="space-y-1">
+                    <label htmlFor="password">
+                        Password
+                    </label>
+                    <input type="password"
+                    id="password"
+                    autoComplete="current-password"
+                    {...register("password")}
+                    className="w-full px-4 h-12 rounded-lg 
+                    border border-gray-400
+                    text-white
+                    focus:border-foreground/30"
+                    placeholder="******" 
+                     />
+                    {errors.password?.message && (
+                        <p className="text-xs text-red-600">{errors.password.message}</p>
+                    )}
+                </div>
+
+                <button
+                type="submit"
+                disabled= {isSubmitting || pending}
+                className="h-10 w-full rounded-md bg-foreground text-background text-sm font-semibold hover:opacity-90 disabled:opacity-60">
+                {isSubmitting || pending ? "Logging in..." : "Log in"}
+                </button>
             </div>
         </form>
     )
--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -7,19 +7,19 @@ export default function AuthLayout({children}: {children: React.ReactNode}) {
             
    
 
-         <div className="grid h-full w-full max-w-4xl grid-cols-1 rounded-xl bg-white/50 dark:bg-black/20 shadow-lg ring-1 ring-black/5 md:grid-cols-2">
+         <div className="grid h-full w-full max-w-4xl grid-cols-1 rounded-xl bg-white/50 dark:bg-black/20 shadow-lg ring-1 ring-black/5 md:grid-cols-2 border">
         
-        {/* Left: Illustration */}
-        <div className="relative hidden md:block">
+ 
+        <div className="relative hidden md:block rounded-l-xl overflow-hidden">
           <Image
-            src="/images/image3.png"
-            alt="Happy pet illustration"
+            src="/images/login.png"
+            alt="pet image"
             fill
-            className="object-cover rounded-l-xl"
+            className="object-fill "
           />
         </div>
 
-     
+
         <div className="flex flex-col items-center justify-center p-8 sm:p-12">
           {children}
         </div>
