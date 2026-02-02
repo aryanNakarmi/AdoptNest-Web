993f08f811e62510540632ba32d6cbd03f8190cb
Aryan <aaryan.nakarmi111@gmail.com>
2026-02-02 21:53:15 +0545

    endpoint updated

--- lib/api/endpoints.ts
+++ lib/api/endpoints.ts
@@ -3,5 +3,10 @@ export const API = {
     AUTH: {
         LOGIN: '/api/v1/auth/login',
         REGISTER: '/api/v1/auth/register',
+    },
+    ADMIN:{
+        USER:{
+            CREATE: '/api/admin/users',
+        }
     }
 }
